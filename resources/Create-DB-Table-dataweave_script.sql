c63546d858ab2e6aef036a32d194aa08c7d4a4d8
abelisle_sfemu <abelisle@mulesoft.com>
2024-09-26 15:21:58 -0700

    Bug fixes and minor improvements

--- resources/Create-DB-Table-dataweave_script.sql
+++ resources/Create-DB-Table-dataweave_script.sql
@@ -1,13 +1,10 @@
 ----------------------------------------------------------------------------------------------------
 CREATE TABLE public.dataweave_script
 (
-  script_name TEXT NOT NULL,
+  script_name TEXT PRIMARY KEY,
   configuration TEXT NOT NULL,
   dw_script TEXT NOT NULL
 );
-
-ALTER TABLE public.dataweave_script
-  ADD CONSTRAINT dw_script_pkey PRIMARY KEY(script_name);
 
 ALTER TABLE public.dataweave_script
   OWNER TO dbuser;
